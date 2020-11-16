e783e27edf45126cb71b90c20eb55f6c696f2a72
Sal Nast <snast@codingdojo.com>
2020-11-16 12:17:07 -0800

    Updating time display

--- time_display_app/views.py
+++ time_display_app/views.py
@@ -4,6 +4,6 @@ from time import gmtime, strftime
 # Create your views here.
 def index(request):
     context = {
-        "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
+        "time": strftime("%m-%d %H:%M %p", gmtime())
     }
     return render(request,'index.html', context)
